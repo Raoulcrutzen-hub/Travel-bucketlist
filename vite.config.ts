d97b47f9ffe1a6c4cdb7c4f7e427979349b84862
Raoulcrutzen-hub <Raoulcrutzen@hotmail.com>
2025-08-13 12:25:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/travel-bucketlist/',//Travel-bucketlist
+  base: '/travel-bucketlist/',
 })
